a5a5284de0e004b1fef8f1711e3bfe3bc7e01968
workmyrepo <srinuthota.1932@gmail.com>
2025-07-05 19:05:15 +0530

    Update LoginPage.java

--- src/test/java/pages/LoginPage.java
+++ src/test/java/pages/LoginPage.java
@@ -1,13 +1,8 @@
 package pages;
 
-import java.time.Duration;
-
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
-
 import com.aventstack.extentreports.Status;
 import com.aventstack.extentreports.ExtentTest;
 
@@ -18,9 +13,7 @@ public class LoginPage {
     WebDriver driver;
     ExtentTest test;
 
- // this one exists both in headed & headless modes
-    private By homeSignInBtn = By.id("nav-link-accountList");
-
+    private By homeSignInBtn = By.id("nav-link-accountList-nav-line-1");
     private By emailInput = By.id("ap_email_login");
     private By continueBtn = By.id("continue");
     private By passwordInput = By.id("ap_password");
@@ -33,17 +26,8 @@ public class LoginPage {
 
     public void loginToAmazon(String username, String password) {
     	
-    	// in LoginPage.loginToAmazon(), before waitForElementClickable(...)
-    	try {
-    	    By cookieAccept = By.id("sp-cc-accept");             // Amazon's “Accept Cookies” button
-    	    WebElement acceptBtn = new WebDriverWait(driver, Duration.ofSeconds(5))
-    	        .until(ExpectedConditions.elementToBeClickable(cookieAccept));
-    	    acceptBtn.click();
-    	    test.log(Status.INFO, "Closed cookie banner");
-    	} catch (Exception ignored) { /* no cookie banner shown */ }
-
     	
-    	WebElement HomeSignInButton = WaitUtil.waitForElementPresent(driver, homeSignInBtn, 20);
+        WebElement HomeSignInButton = WaitUtil.waitForElementClickable(driver, homeSignInBtn, 20);
         test.log(Status.INFO, "<b>Clicked Sign In on Home Page </b>");
         HomeSignInButton.click();
         ReportHelper.logStepWithScreenshot(driver, test);
@@ -65,17 +49,9 @@ public class LoginPage {
         test.log(Status.INFO, "<b>Secured Password Entry : </b>");
         //test.log(Status.INFO, "Entered password. (masked)");
         ReportHelper.logStepWithScreenshot(driver, test);
-        
-        By primary = By.id("nav-link-accountList-nav-line-1");
-        By fallback = By.id("nav-link-accountList");
-        WebElement signInBtn;
-        try {
-            signInBtn = WaitUtil.waitForElementClickable(driver, primary, 20);
-        } catch (Exception e) {
-            test.log(Status.INFO, "Primary sign‑in ID failed, falling back to nav-link-accountList");
-            signInBtn = WaitUtil.waitForElementClickable(driver, fallback, 20);
-        }
-        signInBtn.click();
+
+        WebElement signInButton = WaitUtil.waitForElementClickable(driver, signInBtn, 20);
+        signInButton.click();
        // test.log(Status.INFO, "Clicked Sign In button");
         ReportHelper.logStepWithScreenshot(driver, test);
     }
